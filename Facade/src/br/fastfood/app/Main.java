1dc8ba7fbb339cdb1d7b838f06862a2dd8642387
Biasds <biancasilvasantos02@gmail.com>
2025-04-22 22:49:09 -0300

    Update Main.java
    Mudança na main

--- Facade/src/br/fastfood/app/Main.java
+++ Facade/src/br/fastfood/app/Main.java
@@ -1,9 +1,4 @@
-// após implementar as classes solicitada remova os comentarios
-// para executar o código
-
-package br.fastfood.app;
 import java.util.Scanner;
-//import br.fastfood.facade.ComboFacade;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,14 +9,9 @@ public class Main {
         System.out.print("Sua escolha: ");
         int escolha = in.nextInt();
 
-        /*
-
-         ComboFacade facade = new ComboFacade();
+        ComboFacade facade = new ComboFacade();
         facade.criarCombo(escolha);
         facade.exibirItens();
         System.out.printf("Total a pagar: R$ %.2f%n", facade.getPrecoTotal());
-
-        */
-
     }
 }
